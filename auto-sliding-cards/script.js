d3c7a201a3a0f1bf6d5e15935169b6688de192db
Karan Kaushik <karan.kaushik@searce.com>
2021-06-19 07:22:32 +0530

    auto-sliding-cards

---
+++ auto-sliding-cards/script.js
@@ -0,0 +1,25 @@
+const panels = document.querySelectorAll(".panel");
+
+function removeActiveClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClass();
+    panel.classList.add("active");
+    clearInterval();
+  });
+});
+
+let panel_item = 0;
+setInterval(() => {
+  removeActiveClass();
+  panels[panel_item].classList.add("active");
+  panel_item++;
+  if (panel_item === panels.length) {
+    panel_item = 0;
+  }
+}, 7000);
